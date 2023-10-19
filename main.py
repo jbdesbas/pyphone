4ecc2209a352eaceee8ed326bc1415fe570359e4
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2023-10-19 10:20:21 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import network
+from machine import RTC, I2C, Pin, unique_id, reset
+from config import *
+import ubinascii
+import utime
+import ntptime
+from lib.dfplayer import DFPlayer
+from random import getrandbits
+
+CONFIG={}
+
+def do_connect(ssid,pwd):
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(ssid, pwd)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+rtc = RTC()
+
+mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode() #client id ?
+client_id = mac
+print('MAC :', mac)
+
+df=DFPlayer(uart_id=1) # D4
+
+while True :
+    do_connect(WIFI_SSID, WIFI_PASSWORD)      
+    #df.play(1,int(getrandbits(1))+1)
+    utime.sleep(60)
+
