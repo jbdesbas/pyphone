cee7b1fa6ff286816805fa77347a092908ebaa89
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2026-07-27 21:19:42 +0200

    dockerize

--- server/main.py
+++ server/main.py
@@ -18,7 +18,11 @@ from menu import handle_menu
 
 load_dotenv()
 
-BASE_DIR = Path(getenv("BASE_DIR"))
+BASE_DIR = Path(getenv("BASE_DIR", getenv("SOUND_DIR")))
+
+if not BASE_DIR:
+    raise RuntimeError("BASE_DIR ou SOUND_DIR doit être défini")
+
 SAMPLE_RATE = 8_000
 
 
